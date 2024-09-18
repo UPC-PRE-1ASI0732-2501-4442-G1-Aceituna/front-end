b74182ce1eed2ed1d7444b3affe8726294eb65ae
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2024-09-18 04:09:07 -0500

    feat: added information vehicle component

--- src/app/public/pages/vehicles/vehicles.component.ts
+++ src/app/public/pages/vehicles/vehicles.component.ts
@@ -1,13 +1,40 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input, OnInit} from '@angular/core';
 import {Vehicle} from "../../../movilizing/model/vehicle.entity";
+import {VehicleService} from "../../../movilizing/services/vehicle.service";
+import {NgForOf} from "@angular/common";
+import {LogoApiService} from "../../../shared/services/logo-api.service";
+import {MatCardImage} from "@angular/material/card";
 
 @Component({
   selector: 'app-vehicles',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    MatCardImage
+  ],
   templateUrl: './vehicles.component.html',
   styleUrl: './vehicles.component.css'
 })
-export class VehiclesComponent {
-@Input() vehicle!: Vehicle;
+export class VehiclesComponent implements OnInit {
+  protected vehicleData: Vehicle[] = [];
+  private vehicleService: VehicleService = inject(VehicleService);
+  private Logo = inject(LogoApiService);
+
+  getLogoUrl(url: string) {
+    return this.Logo.getUrlToLogo(url);
+  }
+
+
+  ngOnInit(): void {
+    this.getAllVehicles();
+  }
+
+  private getAllVehicles() {
+    this.vehicleService.getAll().subscribe((response: Vehicle[]) => {
+      console.log(response);
+      this.vehicleData = response;  // Asigna la respuesta (que es un arreglo) a vehicleData
+    });
+  }
+
+
 }
