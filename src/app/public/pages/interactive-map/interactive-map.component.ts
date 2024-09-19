e2eebb24802d139b8c040eeb7c339f56b6f3169a
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2024-09-18 23:03:32 -0500

    feat: added map google

--- src/app/public/pages/interactive-map/interactive-map.component.ts
+++ src/app/public/pages/interactive-map/interactive-map.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GoogleMap} from "@angular/google-maps";
 
 @Component({
   selector: 'app-interactive-map',
   standalone: true,
-  imports: [],
+  imports: [GoogleMap],
   templateUrl: './interactive-map.component.html',
   styleUrl: './interactive-map.component.css'
 })
-export class InteractiveMapComponent {
+export class InteractiveMapComponent implements OnInit{
 
+  ngOnInit(): void {
+    const CONFIGURATION = {
+      locations: [
+        { title: "Escondido Grill", address1: "601 Geary St", address2: "San Francisco, CA 94102, USA", coords: { lat: 37.7866, lng: -122.4133 }, placeId: "ChIJ5Sz52pGAhYAR1raOybzuDp8", actions: [{ label: "Reserve a table", defaultUrl: "https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png" }] },
+        { title: "Escondido Grill", address1: "747 3rd St", address2: "San Francisco, CA 94107, USA", coords: { lat: 37.7785, lng: -122.392 }, placeId: "ChIJeyVKpNd_j4ARLUED10OEr1s", actions: [{ label: "Reserve a table", defaultUrl: "https://www.gstatic.com/pantheon/images/maps/reserve_with_google_preview_v1.png" }] },
+        // Resto de ubicaciones...
+      ],
+      mapOptions: { center: { lat: 38.0, lng: -100.0 }, fullscreenControl: true, mapTypeControl: false, streetViewControl: false, zoom: 4, zoomControl: true, maxZoom: 17, mapId: "" },
+      mapsApiKey: "YOUR_API_KEY_HERE",
+      capabilities: { input: true, autocomplete: true, directions: false, distanceMatrix: true, details: false, actions: true }
+    };
+
+    document.addEventListener('DOMContentLoaded', async () => {
+      await customElements.whenDefined('gmpx-store-locator');
+      const locator = document.querySelector('gmpx-store-locator');
+      locator?.configureFromQuickBuilder(CONFIGURATION);
+    });
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
 // Server Base Path for Fake REST API
-  serverBasePath: 'http://localhost:3000/api/v1'
+  serverBasePath: 'http://localhost:3000/api/v1',
+  keyGoogle:'AIzaSyDMV3i8LKs4cJYqnT0I_hwv-mw_u55x4VE'
 // Server Base Path for Spring Boot REST API
 //serverBasePath: 'http://localhost:8080/api/v1'
 };
