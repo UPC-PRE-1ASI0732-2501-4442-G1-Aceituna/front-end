09d5c9822742c8f3d10f4e6bcaae2a5a4f2b9ed5
ErickVasquezz <89364442+ErickVasquezz@users.noreply.github.com>
2024-09-23 09:06:25 -0400

    feature: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {VehiclesComponent} from "./public/pages/vehicles/vehicles.component";
 import {VehiclePostComponent} from "./public/pages/vehicle-post/vehicle-post.component";
 import {ProfilePageComponent} from "./public/pages/profile-page/profile-page.component";
 import {DashboardPageComponent} from "./public/pages/dashboard-page/dashboard-page.component";
+import {PlanesPageComponent} from "./public/pages/planes-page/planes-page.component";
 
 export const routes: Routes = [
   {path:'myVehicles', component: VehiclesComponent},
   {path: 'postVehicle', component: VehiclePostComponent},
+  { path: 'plans', component: PlanesPageComponent},
   {path: 'dashboard', component: DashboardPageComponent,
     children:[
       {
