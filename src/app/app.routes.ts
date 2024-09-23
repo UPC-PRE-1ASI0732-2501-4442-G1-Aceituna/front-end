98d085e8814796f216c6f991482302f875d45157
ErickVasquezz <89364442+ErickVasquezz@users.noreply.github.com>
2024-09-23 08:36:21 -0400

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import {VehiclesComponent} from "./public/pages/vehicles/vehicles.component";
 import {VehiclePostComponent} from "./public/pages/vehicle-post/vehicle-post.component";
+import {ProfilePageComponent} from "./public/pages/profile-page/profile-page.component";
+import {DashboardPageComponent} from "./public/pages/dashboard-page/dashboard-page.component";
 
 export const routes: Routes = [
   {path:'myVehicles', component: VehiclesComponent},
-  {path: 'postVehicle', component: VehiclePostComponent}
+  {path: 'postVehicle', component: VehiclePostComponent},
+  {path: 'dashboard', component: DashboardPageComponent,
+    children:[
+      {
+        path: 'profile', // Rutas hijas
+        component: ProfilePageComponent
+      }
+    ]
+  }
 ];
