1534f8bf5adc89442790754f4d046bfa918aba09
Beth Oneglio <106390542+bethnlg@users.noreply.github.com>
2024-09-25 09:13:22 -0500

    feat: merge register university student component

---
+++ src/app/public/pages/register-university-student/register-university-student.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-university-student',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-university-student.component.html',
+  styleUrl: './register-university-student.component.css'
+})
+export class RegisterUniversityStudentComponent {
+
+}
