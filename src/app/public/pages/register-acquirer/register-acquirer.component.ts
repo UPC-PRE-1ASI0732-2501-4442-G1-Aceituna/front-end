c05345a856a0e6d27c0eae7fe3d1e0d246a51f65
Beth Oneglio <106390542+bethnlg@users.noreply.github.com>
2024-09-25 09:13:12 -0500

    feat: merge register acquirer component

---
+++ src/app/public/pages/register-acquirer/register-acquirer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-acquirer',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-acquirer.component.html',
+  styleUrl: './register-acquirer.component.css'
+})
+export class RegisterAcquirerComponent {
+
+}
