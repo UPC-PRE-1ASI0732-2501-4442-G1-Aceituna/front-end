b69a9b6c0f01f2e4bd6810d2afb0727030ddcd2b
Juan Calisaya <u202121935@upc.edu.pe>
2024-09-24 21:55:42 -0500

    feat: updated index.html and app.component.html

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
 // Server Base Path for Fake REST API
-  serverBasePath: 'https://my-json-server.typicode.com/DavidRm1911/jsonserverEcomovil',
+  serverBasePath: 'http://localhost:3000/api/v1',
+  //serverBasePath: 'https://my-json-server.typicode.com/DavidRm1911/jsonserverEcomovil',
   keyGoogle:'AIzaSyDMV3i8LKs4cJYqnT0I_hwv-mw_u55x4VE'
 // Server Base Path for Spring Boot REST API
 //serverBasePath: 'http://localhost:8080/api/v1'
